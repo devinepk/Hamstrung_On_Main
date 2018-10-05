7e6b5f27ef1eea6f71766a2443036c5e95bcddb2
Philip Devine <devine.pk@gmail.com>
2018-10-05 10:25:16 -0400

    adds more js for json

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,28 +4,25 @@ let apiRequest = new XMLHttpRequest();
 let output_menu = document.getElementById('output_menu');
 
 document.onreadystatechange = function () {
-    if (document.readyState == "interactive") {
-         getMenu ();
-    }
-}
+  if (document.readyState == "interactive") {
+    getMenu();
+  }
+};
 
 function getMenu() {
 
-  let url = 'https://entree-f18.herokuapp.com/v1/menu'
-
+  let url = 'https://entree-f18.herokuapp.com/v1/menu';
 
   apiRequest.onload = onSuccess;
   apiRequest.onerror = onError;
   apiRequest.open('get', url, true);
   apiRequest.send();
-
 }
 
 function onError() {
 
   if (apiRequest.responseText) {
-      document.getElementById('error').innerHTML = "Our chefs are updating the menu.  Please check back soon.";
-
+    document.getElementById('error').innerHTML = "Our chefs are updating the menu.  Please check back soon.";
   }
 }
 
@@ -35,6 +32,6 @@ function onSuccess() {
 
     let response = JSON.parse(apiRequest.responseText);
 
-    output_menu = document.innerHTML = (response);
+    output_menu = document.innerHTML = response;
   }
 }
